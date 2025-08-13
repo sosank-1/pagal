73fc2892a47e4ad5b6cb145d49ad022cd52a0c50
Sosank Sarmah <sosanksarmah@gmail.com>
2025-08-13 22:59:07 +0530

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnOrm7pHWFjQy5hk8x8VtXTMZmAplaFlM",
+  authDomain: "cylo-b89dc.firebaseapp.com",
+  projectId: "cylo-b89dc",
+  storageBucket: "cylo-b89dc.firebasestorage.app",
+  messagingSenderId: "451560803879",
+  appId: "1:451560803879:web:3ae36b338e7a0056294802"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
